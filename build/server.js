7b8bc7bb50ddd14841acbd46d2ff8312d524ddf9
Matthew Long <MattLong87@gmail.com>
2017-05-02 00:31:57 -0400

    Started mongoose integration

--- build/server.js
+++ build/server.js
@@ -4,30 +4,41 @@ const express = require("express");
 const morgan = require("morgan");
 const apiRouter_1 = require("./routes/apiRouter");
 exports.app = express();
+const config_1 = require("./config");
+const mongoose = require("mongoose");
+//use global promise instead of mongoose's
+mongoose.promise = global.Promise;
 exports.app.use(morgan('common'));
 exports.app.use('/api', apiRouter_1.router);
 let server;
-function runServer() {
-    const port = 8080;
+function runServer(databaseUrl = config_1.DATABASE_URL, port = config_1.PORT) {
     return new Promise((resolve, reject) => {
-        server = exports.app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err);
+        mongoose.connect(databaseUrl, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = exports.app.listen(port, () => {
+                console.log(`Your app is listening on port ${port}`);
+                resolve(server);
+            })
+                .on('error', err => {
+                mongoose.disconnect();
+                reject(err);
+            });
         });
     });
 }
 exports.runServer = runServer;
 function closeServer() {
-    return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-            if (err) {
-                reject(err);
-                return;
-            }
-            resolve();
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
         });
     });
 }
--- ts/server.ts
+++ ts/server.ts
@@ -1,35 +1,46 @@
 import * as express from 'express';
 import * as morgan from 'morgan';
-import {router as apiRouter} from './routes/apiRouter';
+import { router as apiRouter } from './routes/apiRouter';
 export const app = express();
+import { DATABASE_URL, PORT } from './config';
+
+import * as mongoose from 'mongoose';
+//use global promise instead of mongoose's
+mongoose.promise = global.Promise;
 
 app.use(morgan('common'));
 
 app.use('/api', apiRouter);
 
-
-
 let server;
-export function runServer() {
-    const port: number = 8080
-    return new Promise((resolve, reject) => {
-        server = app.listen(port, () => {
-            console.log(`Your app is listening on port ${port}`);
-            resolve(server);
-        }).on('error', err => {
-            reject(err);
+export function runServer(databaseUrl = DATABASE_URL, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve(server);
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
         });
     });
+  })
 }
+
 export function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve();
+  return mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.log('Closing server');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
     });
   });
 }
