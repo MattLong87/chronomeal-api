5b629d788356cf5a8ad840678b4bc471b48730df
Matthew Long <MattLong87@gmail.com>
2017-05-04 18:09:10 -0400

    setup server tests

--- test/test-server.js
+++ test/test-server.js
@@ -1,24 +1,27 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-
+const { TEST_DATABASE_URL } = require('../build/config');
 const { app, closeServer, runServer } = require('../build/server');
-
+const mongoose = require('mongoose');
+mongoose.promise = global.promise;
 const should = chai.should();
 chai.use(chaiHttp);
 
-describe('GET endpoint', function () {
+describe('Foodtracker API', function () {
     before(function () {
-        return runServer();
+        return runServer(TEST_DATABASE_URL);
     });
     after(function () {
         return closeServer();
     });
 
-    it('should return user\'s information on GET', function () {
-        return chai.request(app)
-            .get('/')
-            .then(function (res) {
-                res.should.have.status(200);
-            })
+    describe('GET endpoint', function () {
+        it('should return user\'s information on GET', function () {
+            return chai.request(app)
+                .get('/api')
+                .then(function (res) {
+                    res.should.have.status(200);
+                })
+        })
     })
 })
--- ts/config.ts
+++ ts/config.ts
@@ -1,2 +1,3 @@
 export const DATABASE_URL: string = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/foodtracker';
 export const PORT: number = process.env.PORT || 8080;
+export const TEST_DATABASE_URL: string = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-foodtracker'
