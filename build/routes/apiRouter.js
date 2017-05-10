eb362464934b9dec931142e78faab288e88d069f
Matthew Long <MattLong87@gmail.com>
2017-05-10 11:09:34 -0400

    started implementing Passport

--- build/routes/apiRouter.js
+++ build/routes/apiRouter.js
@@ -2,9 +2,33 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 exports.router = express.Router();
+const session = require("express-session");
 const models_1 = require("../models");
+const passport_1 = require("passport");
+const LocalStrategy = require('passport-local').Strategy;
+passport_1.passport.use(new LocalStrategy((username, password, done) => {
+    models_1.User.findOne({ username: username }, (err, user) => {
+        if (err) {
+            return done(err);
+        }
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username' });
+        }
+        if (!user.validPassword(password)) {
+            return done(null, false, { message: 'Incorrect password' });
+        }
+        return done(null, user);
+    });
+}));
+//MIDDLEWARE
+exports.router.use(session({ secret: 'mydogsnameisarden' }));
+exports.router.use(passport_1.passport.initialize());
+exports.router.use(passport_1.passport.session());
 exports.router.get('/', (req, res) => {
     res.send("foodtracker API");
+});
+exports.router.post('/login', passport_1.passport.authenticate('local'), (req, res) => {
+    res.json({ message: "login successful" });
 });
 //GET a user's information
 exports.router.get('/users/me', (req, res) => {
